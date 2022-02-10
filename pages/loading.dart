0233557332659deca6dd40b6235bc42ad701e882
Bruce Wayne <brucewayne@mail.com>
2022-02-09 22:41:02 -0500

    Time api first test

--- pages/loading.dart
+++ pages/loading.dart
@@ -11,16 +11,23 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
   
-  void getData() async{
-    Response resp = await get(Uri.https('jsonplaceholder.typicode.com', '/posts/1', {'q': 'http'}));
+  void getTime() async{
+    Response resp = await get(Uri.http('worldtimeapi.org', '/api/timezone/Europe/London', {'q': 'http'}));
     Map data = jsonDecode(resp.body);
-    print(data['title']);
+    
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
